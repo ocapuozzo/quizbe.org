575b890f5c98f1cc7b45f869f57f368e13457a90
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2015-08-19 23:40:44 +0200

    a template sub-form for Response prototype : TODO logic range of value

---
+++ src/AppBundle/AppConstants.php
@@ -0,0 +1,19 @@
+<?php
+namespace AppBundle;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AppConstants
+ *
+ * @author kpu
+ */
+class AppConstants {
+   const MIN_VALUE_PROPOSITION = -2;
+   const MAX_VALUE_PROPOSITION = 2;
+   const STEP_VALUE_PROPOSITION = .5;
+}
--- src/AppBundle/Form/ResponseType.php
+++ src/AppBundle/Form/ResponseType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use AppBundle\AppConstants;
 
 class ResponseType extends AbstractType
 {
@@ -14,11 +15,19 @@ class ResponseType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+          $valuesList = array();
+          for ($i = AppConstants::MIN_VALUE_PROPOSITION;
+               $i<= AppConstants::MAX_VALUE_PROPOSITION;
+               $i += AppConstants::STEP_VALUE_PROPOSITION)
+          {
+            $valuesList[$i]=$i;
+          }
+          
         $builder
             ->add('proposition')
             ->add('feedback')
-            ->add('value')
-           // ->add('question')
+            ->add('value', 'choice', array(
+                'choices' => $valuesList))
         ;
     }
     
