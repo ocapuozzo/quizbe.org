5f676eb60155bfa47f6e1379beafaec0c29ccaa4
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2015-10-13 19:28:51 +0200

    send message notifaction when post comment  + redirect home page with user locale

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,8 +3,11 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Question;
 
 class DefaultController extends Controller
 {
@@ -14,7 +17,9 @@ class DefaultController extends Controller
      */
     public function homeAction(Request $request)
     { 
-      return $this->render('default/index.html.twig', array());
+      $locale = $request->getLocale();
+      return $this->redirectToRoute("homepage", array("_locale" => $locale));
+      //return $this->render('default/index.html.twig', array());
     }
     
     /**
@@ -23,5 +28,37 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {             
       return $this->render('default/index.html.twig', array());
+    }
+    
+    
+    /**
+     * @return Json 
+     * @Route("/comment/new/{id}",  name="comment_new")
+     * @Method("GET")
+     */
+    public function commentAction(Request $request, Question $question)
+    {  
+        $user = $this->getDoctrine()->getRepository('AppBundle:User')
+            ->findOneByUsername($question->getDesigner());
+        
+        if (!$user) {
+          return new JsonResponse(array('ok' => 0));
+        }
+        
+        $message = \Swift_Message::newInstance()
+        ->setSubject('QuizBe.org : New comment notification')
+        ->setFrom('admin@quizbe.org')
+        ->setTo($user->getEmail())
+        ->setBody(
+            $this->renderView(
+                // app/Resources/views/Emails/newcomment.txt.twig
+                'Emails/newcomment.txt.twig',
+                array('name' => $question->getName(), 'question' => $question)
+            ),
+            'text/html'
+        );
+        $this->get('mailer')->send($message);
+        
+        return new JsonResponse(array('ok' => 1));
     }
 }
