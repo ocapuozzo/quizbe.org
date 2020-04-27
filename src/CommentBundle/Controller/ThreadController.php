ebaf327b82506d897dc95505e48aed63c6b28972
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2020-04-27 09:21:37 +0200

    (test on sever https - redirect + logger)

--- src/CommentBundle/Controller/ThreadController.php
+++ src/CommentBundle/Controller/ThreadController.php
@@ -595,10 +595,13 @@ class ThreadController extends BaseThreadController
      */
     protected function onCreateCommentSuccess(FormInterface $form, $id, CommentInterface $parent = null)
     {
+        $logger = $this->getContainer()->get('logger');
+
         $v = View::createRouteRedirect('fos_comment_get_thread_comment', array('id' => $id, 'commentId' => $form->getData()->getId()), Response::HTTP_CREATED);
         if (substr($v->getRoute(), 0, 5) == "http:") {
             $v->setRoute('https' . substr($v->getRoute(), 4));
         }
+        $logger->info("onCreateCommentSuccess :" + $v);
         return $v;
     }
 
